4188ddd8d81e3cfdaf5c72d019aa288503d9291e
Baisheng <baisheng@gmail.com>
2019-08-15 17:09:58 +0800

    update

---
+++ src/pages/project/mpcast.md
@@ -0,0 +1,34 @@
+---
+templateKey: project
+sort: 1
+name: mpcast
+title: 播客小程序
+subtitle: 基于微信小程序的音频课程解决方案
+summary: 为课程主播提供轻量化、独立运营的播客解决方案
+workType:
+  - 产品策划
+  - 设计
+  - 研发
+primaryColor: '#9db2df'
+hero:
+  publicURL: /img/products-full-width.jpg
+sections:
+  - type: text
+    header: 介绍
+    body: 为课程主播提供轻量化、独立运营的播客解决方案
+  - "body": "<p>Spacetime let’s you see where your calendar overlaps with everyone on your remote team, without ever leaving Slack.</p>\n<p>As a remote worker, you’re used to seeing your teammates in two places: Slack and team meetings. But when multiple time zones are involved, getting people together (at the right time) isn’t always easy. With Spacetime, you can see the time, location, and weather for every member of your remote team, without ever leaving your workspace.</p>\n"
+    "bodySecondary": "<ul><li>Seamless Slack chat integration</li><li>Work hours visual editor</li></ul>"
+    "header": "Execution"
+    "type": "text" 
+  - columns: 1
+    frameColor: '#202429'
+    images:
+      - /img/mpcast-1.png
+    staggered: true
+    staggeredCopyHtml: >-
+      <h2> 清晰明快的 <span style=\"color:
+      #ab2b94\">入口</span></h2>\n<p>入口页清晰的版块划分与交互让用户快速定位资源</p>
+    staggeredStart: right
+    type: imagegrid
+---
+
--- src/templates/project/modules/Text/index.js
+++ src/templates/project/modules/Text/index.js
@@ -4,73 +4,76 @@ import * as classnames from "classnames"
 import PropTypes from "prop-types"
 
 const replaceBullets = str => str.replace(
-  /<li>(.+?)<\/li>/g,
-  "<li><svg width=\"10\" height=\"16\"><use xlink:href=\"#bullet\" /></svg><span>$1</span></li>",
+    /<li>(.+?)<\/li>/g,
+    "<li><svg width=\"10\" height=\"16\"><use xlink:href=\"#bullet\" /></svg><span>$1</span></li>",
 )
-
+// const replaceBullets = (str) => {
+//     console.log(str)
+//     return str
+// }
 const Text = ({
-                className, header, body, bodySecondary,
+                  className, header, body, bodySecondary,
               }) => (
-  <section
-    className={classnames(
-      className,
-      styles.container,
-      {
-        [styles.noHeader]: !header,
-      },
-    )}
-  >
-    {header && (<span>{header}</span>)}
-    {bodySecondary
-      ? (
-        <div className={classnames(styles.body, styles.hasSecondary)}>
-          <p dangerouslySetInnerHTML={{ __html: body.split("</p>", 1)[0].replace(/^<p>/, "") }}/>
-          <div
-            className={styles.remainder}
-            dangerouslySetInnerHTML={{
-              __html: replaceBullets(
-                body
-                  .split("</p>")
-                  .slice(1)
-                  .join("</p>"),
-              ),
-            }}>
-            <div dangerouslySetInnerHTML={{
-              __html: replaceBullets(bodySecondary),
-            }}/>
-          </div>
-        </div>
-      )
-      : (
-        <div className={styles.body}>
-          <p dangerouslySetInnerHTML={{ __html: body.split("</p>, 1")[0].replace(/^<p>/, "") }}/>
-          <div
-            className={styles.remainder}
-            dangerouslySetInnerHTML={{
-              __html: replaceBullets(
-                body
-                  .split("</p>")
-                  .slice(1)
-                  .join("</p>"),
-              ),
-            }}
-          />
-        </div>
-      )}
-  </section>
+    <section
+        className={classnames(
+            className,
+            styles.container,
+            {
+                [styles.noHeader]: !header,
+            },
+        )}
+    >
+        {header && (<span>{header}</span>)}
+        {bodySecondary
+            ? (
+                <div className={classnames(styles.body, styles.hasSecondary)}>
+                    <p dangerouslySetInnerHTML={{__html: body.split("</p>", 1)[0].replace(/^<p>/, "")}}/>
+                    <div
+                        className={styles.remainder}
+                        dangerouslySetInnerHTML={{
+                            __html: replaceBullets(
+                                body
+                                    .split("</p>")
+                                    .slice(1)
+                                    .join("</p>"),
+                            ),
+                        }}>
+                    <div dangerouslySetInnerHTML={{
+                        __html: replaceBullets(bodySecondary),
+                    }}/>
+                    </div>
+                </div>
+            )
+            : (
+                <div className={styles.body}>
+                    <p dangerouslySetInnerHTML={{__html: body.split("</p>, 1")[0].replace(/^<p>/, "")}}/>
+                    <div
+                        className={styles.remainder}
+                        dangerouslySetInnerHTML={{
+                            __html: replaceBullets(
+                                body
+                                    .split("</p>")
+                                    .slice(1)
+                                    .join("</p>"),
+                            ),
+                        }}
+                    />
+                </div>
+            )}
+    </section>
 )
 
 Text.propTypes = {
-  className: PropTypes.string,
-  header: PropTypes.string,
-  body: PropTypes.string.isRequired,
-  bodySecondary: PropTypes.string,
+    className: PropTypes.string,
+    header: PropTypes.string,
+    body: PropTypes.string.isRequired,
+    bodySecondary: PropTypes.string,
 }
 
 Text.defaultProps = {
-  className: "",
-  header: "",
-  bodySecondary: null,
+    className: "",
+    header: "",
+    bodySecondary: null,
 }
 
 export default Text
